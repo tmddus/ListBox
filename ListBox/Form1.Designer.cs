e187b0fbb813b85e5bdbbb773cd1d96af01c082d
Seungyeon Song <song99pro@gmail.com>
2019-05-21 11:57:05 +0900

    #2 코드 구현

--- ListBox/Form1.Designer.cs
+++ ListBox/Form1.Designer.cs
@@ -42,6 +42,7 @@
             this.lbView.Name = "lbView";
             this.lbView.Size = new System.Drawing.Size(245, 100);
             this.lbView.TabIndex = 0;
+            this.lbView.SelectedIndexChanged += new System.EventHandler(this.LbView_SelectedIndexChanged);
             // 
             // lblResult
             // 
@@ -67,6 +68,7 @@
             this.btnAdd.TabIndex = 3;
             this.btnAdd.Text = "추가";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
             // 
             // Form1
             // 
@@ -81,6 +83,7 @@
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "리스트추가";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- ListBox/Form1.cs
+++ ListBox/Form1.cs
@@ -12,9 +12,34 @@ namespace ListBox
 {
     public partial class Form1 : Form
     {
+        private string OrgStr = "";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.OrgStr = this.lblResult.Text;
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            if(this.txtList.Text != "")
+            {
+                this.lbView.Items.Add(this.txtList.Text);
+                this.txtList.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("아이템을 입력하세요", "알림", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                this.txtList.Focus();
+            }
+        }
+
+        private void LbView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.lblResult.Text = this.OrgStr + this.lbView.SelectedItem.ToString();
         }
     }
 }
